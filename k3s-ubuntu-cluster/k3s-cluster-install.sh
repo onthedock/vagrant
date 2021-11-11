65599db70785b7cade6d441f74ceaad87ccec02a
Xavi Aznar <xavi.aznar@outlook.com>
2021-11-11 21:26:22 +0100

    Included parameter to upgrade major version of K3s

--- k3s-ubuntu-cluster/k3s-cluster-install.sh
+++ k3s-ubuntu-cluster/k3s-cluster-install.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
-export IPControlPlaneNode=192.168.1.101
-export IPWorkerNode1=192.168.1.102
-export IPWorkerNode2=192.168.1.103
-export REMOTE_USER=operador
+IPControlPlaneNode=192.168.1.101
+IPWorkerNode1=192.168.1.102
+IPWorkerNode2=192.168.1.103
+REMOTE_USER=operador
+K3S_VERSION="v1.22.3+k3s1"
 
 # Install the ControlPlane
-k3sup install --ip $IPControlPlaneNode --user $REMOTE_USER --k3s-extra-args='--flannel-iface enp0s8'
+k3sup install --ip $IPControlPlaneNode \
+              --user $REMOTE_USER \
+              --k3s-extra-args='--flannel-iface enp0s8' \
+              --k3s-version $K3S_VERSION
+              
 # Install the agents/worker nodes
-k3sup join --ip $IPWorkerNode1 --server-ip $IPControlPlaneNode --user $REMOTE_USER --k3s-extra-args='--flannel-iface enp0s8'
-k3sup join --ip $IPWorkerNode2 --server-ip $IPControlPlaneNode --user $REMOTE_USER --k3s-extra-args='--flannel-iface enp0s8'
+k3sup join --ip $IPWorkerNode1 \
+           --server-ip $IPControlPlaneNode\
+           --user $REMOTE_USER\
+           --k3s-extra-args='--flannel-iface enp0s8' \
+           --k3s-version $K3S_VERSION
+
+k3sup join --ip $IPWorkerNode2 \
+           --server-ip $IPControlPlaneNode \
+           --user $REMOTE_USER \
+           --k3s-extra-args='--flannel-iface enp0s8' \
+           --k3s-version $K3S_VERSION
