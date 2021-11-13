50e54a821732e34b0d2c196286c4e4e53cebd211
Xavi Aznar <xavi.aznar@outlook.com>
2021-11-13 21:26:51 +0100

    Fix issue #11 - Finalizar el script si isHelmInstalled no encuentra KUBECONFIG definido

--- k3s-ubuntu-cluster/deploy-longhorn-using-helm.sh
+++ k3s-ubuntu-cluster/deploy-longhorn-using-helm.sh
@@ -9,6 +9,10 @@ function getKubeconfig {
     elif test -f $HOME/.kube/config
     then
         echo "Using default kubeconfig at $HOME/.kube/config"
+    elif [[ -z "$KUBECONFIG" ]]
+    then
+        echo "ERROR - Unable to find a valid kubeconfig"
+        exit 1
     else
         echo "Using \$KUBECONFIG=$KUBECONFIG"
     fi
