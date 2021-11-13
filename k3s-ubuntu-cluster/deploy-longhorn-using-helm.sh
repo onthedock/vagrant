0bd10dff081fd99bbd65f6816fc6be4761181165
Xavi Aznar <xavi.aznar@outlook.com>
2021-11-13 22:43:42 +0100

    Refactor setDefaultStorageClass
    La función establece la anotación is-default-class=true para la
    storageClass  especificada además de el valor false para el resto.

--- k3s-ubuntu-cluster/deploy-longhorn-using-helm.sh
+++ k3s-ubuntu-cluster/deploy-longhorn-using-helm.sh
@@ -41,8 +41,11 @@ function setDefaultStorageClass {
 
     for storageclass in $storageClassList
     do
-        if [ "$storageclass" != "$defaultStorageClass" ]
+        if [ "$storageclass" = "$defaultStorageClass" ]
         then
+            echo "Set default storageClass for $storageclass"
+            kubectl patch storageclass $storageclass -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+        else
             echo "Removing default storageClass for $storageclass"
             kubectl patch storageclass $storageclass -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
         fi
