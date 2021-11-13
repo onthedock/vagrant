043d17af5cb7ffc51d6757fd917a28b4e2028c07
Xavi Aznar <xavi.aznar@outlook.com>
2021-11-13 19:50:16 +0100

    Script to automate Longhorn installation (using Helm)

---
+++ k3s-ubuntu-cluster/deploy-longhorn-using-helm.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+function getKubeconfig {
+    scriptDir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+    if test -f "kubeconfig"
+    then
+        echo "Using $scriptDir/kubeconfig"
+        export KUBECONFIG=$scriptDir/kubeconfig
+    elif test -f $HOME/.kube/config
+    then
+        echo "Using default kubeconfig at $HOME/.kube/config"
+    else
+        echo "Using \$KUBECONFIG=$KUBECONFIG"
+    fi
+}
+
+
+function isHelmAppInstalled {
+    helmChart="$1"
+    helmRepoChart="$2"
+    chartNamespace="$3"
+
+    checkRelease=$(helm status $helmChart --namespace $chartNamespace 2>/dev/null| grep -i status | awk '{ print $2 }')
+
+    if [ "$checkRelease" != "deployed" ]
+    then
+        echo "...Installing longhorn"
+        helm install $helmChart $helmRepoChart --namespace $chartNamespace --create-namespace
+    else
+        echo "... $helmChart is already installed in the namespace $chartNamespace"
+    fi
+}
+
+function main {
+    getKubeconfig
+
+    # Those commands are idempotent
+    helm repo add longhorn https://charts.longhorn.io
+    helm repo update
+    
+    isHelmAppInstalled "longhorn" "longhorn/longhorn" "longhorn-system"
+}
+
+main
