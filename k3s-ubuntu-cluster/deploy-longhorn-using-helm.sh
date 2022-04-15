b271ac8f7869686e4aaa32af61cfcd46bd1ccc67
Xavi Aznar <xavi.aznar@outlook.com>
2022-04-15 22:10:00 +0200

    Removes warnings from shell-check

--- k3s-ubuntu-cluster/deploy-longhorn-using-helm.sh
+++ k3s-ubuntu-cluster/deploy-longhorn-using-helm.sh
@@ -1,37 +1,32 @@
 #!/usr/bin/env bash
 
 function getKubeconfig {
-    scriptDir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-    if test -f "kubeconfig"
-    then
-        echo "Using $scriptDir/kubeconfig"
+    scriptDir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+    if test -f "kubeconfig"; then
+        printf "Using %s/kubeconfig\n" "$scriptDir"
         export KUBECONFIG=$scriptDir/kubeconfig
-    elif test -f $HOME/.kube/config
-    then
-        echo "Using default kubeconfig at $HOME/.kube/config"
-    elif [[ -z "$KUBECONFIG" ]]
-    then
+    elif test -f "$HOME"/.kube/config; then
+        printf "Using default kubeconfig at %s/.kube/config\n" "$HOME"
+    elif [[ -z "$KUBECONFIG" ]]; then
         echo "ERROR - Unable to find a valid kubeconfig"
         exit 1
     else
-        echo "Using \$KUBECONFIG=$KUBECONFIG"
+        printf "Using \$KUBECONFIG=%s\n" "$KUBECONFIG"
     fi
 }
-
 
 function installHelmChart {
     helmChart="$1"
     helmRepoChart="$2"
     chartNamespace="$3"
 
-    checkRelease=$(helm status $helmChart --namespace $chartNamespace 2>/dev/null| grep -i status | awk '{ print $2 }')
+    checkRelease=$(helm status "$helmChart" --namespace "$chartNamespace" 2>/dev/null | grep -i status | awk '{ print $2 }')
 
-    if [ "$checkRelease" != "deployed" ]
-    then
+    if [ "$checkRelease" != "deployed" ]; then
         echo "...Installing longhorn"
-        helm install $helmChart $helmRepoChart --namespace $chartNamespace --create-namespace
+        helm install "$helmChart" "$helmRepoChart" --namespace "$chartNamespace" --create-namespace
     else
-        echo "... $helmChart is already installed in the namespace $chartNamespace"
+        printf "... %s is already installed in the namespace %s" "$helmChart" "$chartNamespace"
     fi
 }
 
@@ -39,15 +34,13 @@ function setDefaultStorageClass {
     defaultStorageClass="$1"
     storageClassList=$(kubectl get storageclass -o name | awk -F '/' '{print $2}')
 
-    for storageclass in $storageClassList
-    do
-        if [ "$storageclass" = "$defaultStorageClass" ]
-        then
-            echo "Set default storageClass for $storageclass"
-            kubectl patch storageclass $storageclass -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+    for storageclass in $storageClassList; do
+        if [ "$storageclass" = "$defaultStorageClass" ]; then
+            printf "Set default storageClass for %s\n" "$storageclass"
+            kubectl patch storageclass "$storageclass" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
         else
-            echo "Removing default storageClass for $storageclass"
-            kubectl patch storageclass $storageclass -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+            printf "Removing default storageClass for %s\n" "$storageclass"
+            kubectl patch storageclass "$storageclass" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
         fi
     done
 }
@@ -58,7 +51,7 @@ function main {
     # Those commands are idempotent
     helm repo add longhorn https://charts.longhorn.io
     helm repo update
-    
+
     installHelmChart "longhorn" "longhorn/longhorn" "longhorn-system"
     setDefaultStorageClass "longhorn"
 }
