dc774a78f089ac47907e85dce8183ecf015cbe8d
Xavi Aznar <xavi.aznar@outlook.com>
2022-05-09 22:54:36 +0200

    Usa la configuración del Vagrantfile para unir nodos worker al clúster
    - El primer nodo del clúster contiene el ControlPlane (no HA)
    - El resto de nodos son nodos k8s-agent
    - Asigna IPs de manera consecutiva a la IP del nodo del ControlPlane

--- k3s-ubuntu-cluster/k3s_cluster_install.sh
+++ k3s-ubuntu-cluster/k3s_cluster_install.sh
@@ -1,26 +1,39 @@
 #!/usr/bin/env bash
 
 IPControlPlaneNode=192.168.1.101
-IPWorkerNode1=192.168.1.102
-IPWorkerNode2=192.168.1.103
 REMOTE_USER=operador
 K3S_VERSION="v1.23.5+k3s1"
 
 # Install the ControlPlane
 k3sup install --ip $IPControlPlaneNode \
-              --user $REMOTE_USER \
-              --k3s-extra-args='--flannel-iface enp0s8' \
-              --k3s-version $K3S_VERSION
+    --user $REMOTE_USER \
+    --k3s-extra-args='--flannel-iface enp0s8' \
+    --k3s-version $K3S_VERSION
               
-# Install the agents/worker nodes
-k3sup join --ip $IPWorkerNode1 \
-           --server-ip $IPControlPlaneNode\
-           --user $REMOTE_USER\
-           --k3s-extra-args='--flannel-iface enp0s8' \
-           --k3s-version $K3S_VERSION
+# Install the K8s agent nodes
+node_count=$(grep -i 'NodeCount =' Vagrantfile | awk '{print $3}')
+ip_oct_1=$(echo $IPControlPlaneNode | awk -F\. '{print $1}')
+ip_oct_2=$(echo $IPControlPlaneNode | awk -F\. '{print $2}')
+ip_oct_3=$(echo $IPControlPlaneNode | awk -F\. '{print $3}')
+ip_oct_4=$(echo $IPControlPlaneNode | awk -F\. '{print $4}')
 
-k3sup join --ip $IPWorkerNode2 \
-           --server-ip $IPControlPlaneNode \
-           --user $REMOTE_USER \
-           --k3s-extra-args='--flannel-iface enp0s8' \
-           --k3s-version $K3S_VERSION
+for ((i=1; i<node_count; i++));do
+    IP_K8S_AGENT="$ip_oct_1.$ip_oct_2.$ip_oct_3.$((ip_oct_4+i))"
+    k3sup join --ip  "$IP_K8S_AGENT" \
+        --server-ip $IPControlPlaneNode\
+        --user $REMOTE_USER\
+        --k3s-extra-args='--flannel-iface enp0s8' \
+        --k3s-version $K3S_VERSION
+done
+
+# k3sup join --ip $IPWorkerNode1 \
+#            --server-ip $IPControlPlaneNode\
+#            --user $REMOTE_USER\
+#            --k3s-extra-args='--flannel-iface enp0s8' \
+#            --k3s-version $K3S_VERSION
+
+# k3sup join --ip $IPWorkerNode2 \
+#            --server-ip $IPControlPlaneNode \
+#            --user $REMOTE_USER \
+#            --k3s-extra-args='--flannel-iface enp0s8' \
+#            --k3s-version $K3S_VERSION
